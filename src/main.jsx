4ca412cb44899c903acaac98885ef9225293381c
Anand Bhagat <anbhagat1997@gmail.com>
2024-01-30 22:00:34 +0530

    sentry bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import * as Sentry from "@sentry/react";
 
 Sentry.init({
     dsn: `${import.meta.env.VITE_APP_SENTRY_DSN}`,
