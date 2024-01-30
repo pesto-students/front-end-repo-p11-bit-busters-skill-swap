df0c3bf4d97fba9ad9f7a9953ffc92ca534e6a61
Anand Bhagat <anbhagat1997@gmail.com>
2024-01-30 21:53:10 +0530

    added sentry

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 export default ({ mode }) => {
     process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
     return defineConfig({
-        plugins: [react()],
+        build: {
+            sourcemap: true, // Source map generation must be turned on
+        },
+        plugins: [
+            react(),
+            sentryVitePlugin({
+                authToken: process.env.SENTRY_AUTH_TOKEN,
+                org: "anand-bhagat",
+                project: "javascript-react",
+            }),
+        ],
     });
 };
